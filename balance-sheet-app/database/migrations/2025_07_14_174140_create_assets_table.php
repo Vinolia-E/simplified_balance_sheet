7fa1bddda16853d52149116fcf14bf6fc5fc889b
Vinolia-E <vinolia.b.19@gmail.com>
2025-07-14 21:45:07 +0300

    feat: Modify assets and liebilities migration features

--- balance-sheet-app/database/migrations/2025_07_14_174140_create_assets_table.php
+++ balance-sheet-app/database/migrations/2025_07_14_174140_create_assets_table.php
@@ -6,21 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('assets', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->decimal('amount', 15, 2);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('assets');
     }
